d8e43e1e89cbaeda4e7369112f7a826a20083906
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2018-09-20 22:10:29 +1000

    testupdate

--- test03.sh
+++ test03.sh
@@ -19,24 +19,24 @@ test_file_2="file2"
 test_file_3="file3"
 
 
-perl legit.pl init 
+legit.pl init 
 echo 123456 > $test_file_1
 echo abcdefg > $test_file_2
 echo line1 > $test_file_3
-perl legit.pl add $test_file_1 $test_file_2 $test_file_3 
-perl legit.pl commit -m 'commit 1' 
-perl legit.pl rm --force $test_file_2 
-perl legit.pl status 
-perl legit.pl log 
+legit.pl add $test_file_1 $test_file_2 $test_file_3 
+legit.pl commit -m 'commit 1' 
+legit.pl rm --force $test_file_2 
+legit.pl status 
+legit.pl log 
 echo another >> $test_file_1
-perl legit.pl add $test_file_1 $test_file_2 $test_file_3 
-perl legit.pl add $test_file_1 $test_file_3 
-perl legit.pl commit -m commit02 
-perl legit.pl status 
-perl legit.pl rm --cached $test_file_3 
-perl legit.pl status 
-perl legit.pl rm $test_file_3 
-perl legit.pl rm --force $test_file_3 
+legit.pl add $test_file_1 $test_file_2 $test_file_3 
+legit.pl add $test_file_1 $test_file_3 
+legit.pl commit -m commit02 
+legit.pl status 
+legit.pl rm --cached $test_file_3 
+legit.pl status 
+legit.pl rm $test_file_3 
+legit.pl rm --force $test_file_3 
 
 # The expected result are retrived from reference implementation using the same commands
 # expected="expected.txt" 
